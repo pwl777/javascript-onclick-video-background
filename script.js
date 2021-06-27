f07c3e3ee189eebbc14e80064cf0327aa6fafcc0
Pierson Lippard <68010303+pwl777@users.noreply.github.com>
2021-06-27 09:57:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/* ------ JavaScript - Onlick Video Background ------ */
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
+// Preloader
+
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
